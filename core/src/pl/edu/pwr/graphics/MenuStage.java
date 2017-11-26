f1f4a6d2b37d53fccba9ed4288bd75e851842756
Bartosz Cieśla <bartociesla@gmail.com>
2017-11-26 14:41:16 +0100

    More buttons

--- core/src/pl/edu/pwr/graphics/MenuStage.java
+++ core/src/pl/edu/pwr/graphics/MenuStage.java
@@ -18,29 +18,33 @@ public class MenuStage extends Stage {
         shapeRenderer = new ShapeRenderer();
         batch = new SpriteBatch();
 
-        //Create a font
-        BitmapFont font = new BitmapFont();
-        Skin skin = new Skin();
-        skin.add("default", font);
- 
-        //Create a texture
-        Pixmap pixmap = new Pixmap(50, 50, Pixmap.Format.RGB888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-        skin.add("background",new Texture(pixmap));
- 
-        //Create a button style
-        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
-        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
-        textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
-        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
-        textButtonStyle.font = skin.getFont("default");
-        skin.add("default", textButtonStyle);
+        float xPos = midpointX + ((width - buttonWidth) / 2.0f);
+        float yPos = height - buttonHeight - buttonSpacing;
 
-        startButton = new TextButton("START", skin);
-        startButton.setPosition(midpointX, height - 50);
+        startButton = createButton("START");
+        startButton.setPosition(xPos, yPos);
+        yPos -= buttonHeight + buttonSpacing;
+
+        pauseButton = createButton("PAUSE");
+        pauseButton.setPosition(xPos, yPos);
+        yPos -= buttonHeight + buttonSpacing;
+
+        stopButton = createButton("STOP");
+        stopButton.setPosition(xPos, yPos);
+        yPos -= buttonHeight + buttonSpacing;
+
+        jumpButton = createButton("JUMP");
+        jumpButton.setPosition(xPos, yPos);
+        yPos -= buttonHeight + buttonSpacing;
+
+        fastForwardButton = createButton("FAST FORWARD");
+        fastForwardButton.setPosition(xPos, yPos);
+
         this.addActor(startButton);
+        this.addActor(pauseButton);
+        this.addActor(stopButton);
+        this.addActor(jumpButton);
+        this.addActor(fastForwardButton);
     }
 
     private int width;
@@ -49,6 +53,14 @@ public class MenuStage extends Stage {
     private ShapeRenderer shapeRenderer;
     private SpriteBatch batch;
     private TextButton startButton;
+    private TextButton pauseButton;
+    private TextButton stopButton;
+    private TextButton jumpButton;
+    private TextButton fastForwardButton;
+
+    private final static float buttonWidth = 150.0f;
+    private final static float buttonHeight = 50.0f;
+    private final static float buttonSpacing = 10.0f;
 
     @Override
     public void draw() {
@@ -59,6 +71,10 @@ public class MenuStage extends Stage {
         
         batch.begin();
         startButton.draw(batch, 1.0f);
+        pauseButton.draw(batch, 1.0f);
+        stopButton.draw(batch, 1.0f);
+        jumpButton.draw(batch, 1.0f);
+        fastForwardButton.draw(batch, 1.0f);
         batch.end();
     }
 
@@ -66,5 +82,29 @@ public class MenuStage extends Stage {
     public void dispose() {
         shapeRenderer.dispose();
         batch.dispose();
+    }
+
+    private TextButton createButton(String label){
+        // Create a font
+        BitmapFont font = new BitmapFont();
+        Skin skin = new Skin();
+        skin.add("default", font);
+
+        // Create a texture
+        Pixmap pixmap = new Pixmap((int)buttonWidth, (int)buttonHeight, Pixmap.Format.RGB888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("background",new Texture(pixmap));
+
+        // Create a button style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
+        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+
+        return new TextButton(label, skin);
     }
 }
