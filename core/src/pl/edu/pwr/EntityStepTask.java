18ecb94c9c23e842e0d97db20b5436caa5823247
Bartosz Cieśla <bartociesla@gmail.com>
2017-11-26 14:10:35 +0100

    Intersection detection with usage example.

--- core/src/pl/edu/pwr/EntityStepTask.java
+++ core/src/pl/edu/pwr/EntityStepTask.java
@@ -2,8 +2,7 @@ package pl.edu.pwr;
 
 import pl.edu.pwr.graphics.Entity;
 
-import java.util.ArrayList;
-import java.util.TimerTask;
+import java.util.*;
 
 public class EntityStepTask extends TimerTask{
     public EntityStepTask(ArrayList<Entity> entities){
@@ -14,6 +13,20 @@ public class EntityStepTask extends TimerTask{
     public void run() {
         for(Entity e : entities){
             e.makeStep();
+        }
+
+        Map<Entity, Set<Entity>> intersections = Entity.getIntersectedEntities(entities);
+        Iterator it = intersections.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println("Entity: <" + pair.getKey().toString() + "> intersect with: ");
+
+            Set<Entity> set = (Set<Entity>)pair.getValue();
+            for(Entity e : set){
+                System.out.println(" - {" + e.toString() + "}");
+            }
+
+            it.remove(); // avoids a ConcurrentModificationException
         }
     }
 
