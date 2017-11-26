cdd2406a82e74ad2d1912b500a4fed1a0bde6b39
Bartosz Janusz <janusz.bartek@gmail.com>
2017-11-26 18:23:53 +0100

    Removed abstact from Entity

--- core/src/pl/edu/pwr/engine/simulation/Simulation.java
+++ core/src/pl/edu/pwr/engine/simulation/Simulation.java
@@ -12,11 +12,34 @@ public class Simulation {
 
     List<Entity> plants;
     List<Entity> herbivores;
+    List<Entity> deadHerbivores;
     List<Entity> carnivores;
 
     public Simulation() {
         this.plants = Stream.generate(() -> EntityFactory.getEntity(EntityType.PLANT)).limit(Parameters.numberPlants).collect(Collectors.toList());
         this.herbivores = Stream.generate(() -> EntityFactory.getEntity(EntityType.HERBIVORE)).limit(Parameters.numberHerbivores).collect(Collectors.toList());
         this.carnivores = Stream.generate(() -> EntityFactory.getEntity(EntityType.CARNIVORE)).limit(Parameters.numberCarnivores).collect(Collectors.toList());
+    }
+
+    public void simulate() {
+
+        List<Entity> plantsToRemove;
+    }
+
+    public Entity findNearestEntity(Entity entity, List<Entity> entities) {
+
+        float minDistance = Float.MAX_VALUE;
+        Entity closestEntity = null;
+        float x = entity.getX();
+        float y = entity.getY();
+
+        for (Entity entityB : entities) {
+            float distance = (float) Math.sqrt(Math.pow(entityB.getX() - x, 2) + Math.pow(entityB.getY() - y, 2));
+            if(distance < minDistance){
+                minDistance = distance;
+                closestEntity = entityB;
+            }
+        }
+        return closestEntity;
     }
 }
--- core/src/pl/edu/pwr/graphics/Entity.java
+++ core/src/pl/edu/pwr/graphics/Entity.java
@@ -7,7 +7,7 @@ import pl.edu.pwr.engine.Parameters;
 import java.util.*;
 import java.util.function.Supplier;
 
-public abstract class Entity {
+public class Entity {
 
     // ===== PRIVATE =====
     protected final static float maxAngle = 2 * (float) Math.PI;
@@ -79,7 +79,9 @@ public abstract class Entity {
         this.y = y;
     }
 
-    public abstract void setNextInputs(double[] inputs);
+    public void setNextInputs(double[] inputs) {
+    }
+
 
     public float getSpeed() {
         return speed;
