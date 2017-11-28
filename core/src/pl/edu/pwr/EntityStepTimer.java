e558d8fd0cec82861b5d6ee7458af1faf661b4c9
Bartosz Janusz <janusz.bartek@gmail.com>
2017-11-28 15:25:11 +0100

    Genetic algorithm integration

--- core/src/pl/edu/pwr/EntityStepTimer.java
+++ core/src/pl/edu/pwr/EntityStepTimer.java
@@ -1,9 +1,15 @@
 package pl.edu.pwr;
 
+import pl.edu.pwr.engine.simulation.EntityFactory;
+import pl.edu.pwr.engine.simulation.EntityType;
 import pl.edu.pwr.engine.simulation.Simulation;
+import pl.edu.pwr.engine.training.GeneticAlgorithm;
 import pl.edu.pwr.graphics.Entity;
 
+import java.time.Clock;
+import java.time.LocalTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class EntityStepTimer extends ForwardableTimer {
 
@@ -21,12 +27,19 @@ public class EntityStepTimer extends ForwardableTimer {
 
     @Override
     protected void onFinish() {
-        // Nothing to do
+        LocalTime start = LocalTime.now();
+        List<Entity> newHerbivorePopulation = new GeneticAlgorithm(simulation.getHerbivoresGenotypes()).run().stream()
+                .map(x -> EntityFactory.getEntity(EntityType.HERBIVORE, x)).collect(Collectors.toList());
+        List<Entity> newCarnivorePopulation = new GeneticAlgorithm(simulation.getCarnivoresGenotypes()).run().stream()
+                .map(x -> EntityFactory.getEntity(EntityType.CARNIVORE, x)).collect(Collectors.toList());
+        LocalTime end = LocalTime.now();
+        System.out.println("Finished GA: " + (end.getNano() - start.getNano()));
+        this.simulation = new Simulation(newHerbivorePopulation, newCarnivorePopulation);
+        start();
     }
 
     @Override
     protected void onReset() {
-        simulation = new Simulation();
         start();
     }
 
